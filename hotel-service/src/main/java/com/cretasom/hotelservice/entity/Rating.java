d4341d558646c2d8d8595005d1cf95cffe1d3284
crestasom <41513543+shrest0000@users.noreply.github.com>
2023-05-13 10:49:39 +0545

    established communication from user service to rating service

--- hotel-service/src/main/java/com/cretasom/hotelservice/entity/Rating.java
+++ hotel-service/src/main/java/com/cretasom/hotelservice/entity/Rating.java
@@ -9,5 +9,5 @@ public class Rating {
 	private String userId;
 	private String hotelId;
 	private int rating;
-	private String remark;
+	private String feedback;
 }
--- user-service/src/main/java/com/cretasom/userservice/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/cretasom/userservice/service/impl/UserServiceImpl.java
@@ -1,11 +1,17 @@
 package com.cretasom.userservice.service.impl;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.cretasom.hms.exception.ResourceNotFoundException;
+
+import com.cretasom.userservice.entity.Rating;
 import com.cretasom.userservice.entity.User;
 import com.cretasom.userservice.repo.UserRepository;
 import com.cretasom.userservice.service.UserService;
@@ -15,8 +21,9 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
-
+	private final WebClient.Builder webClientBuilder;
 	private UserRepository userRepository;
+	private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
 	public User saveUser(User user) {
 		user.setUserId(UUID.randomUUID().toString());
@@ -28,8 +35,14 @@ public class UserServiceImpl implements UserService {
 	}
 
 	public User getUser(String userId) {
-		return userRepository.findById(userId)
+		User user = userRepository.findById(userId)
 				.orElseThrow(() -> new ResourceNotFoundException("Given ID is not found in server: " + userId));
+		Rating[] ratingResponses = webClientBuilder.build().get().uri("http://rating-service/rating/user/" + userId)
+				.retrieve().bodyToMono(Rating[].class).block();
+		logger.info("inventoryResponses [{}]", ratingResponses[0]);
+		user.setRatingList(Arrays.asList(ratingResponses));
+
+		return user;
 	}
 
 }
