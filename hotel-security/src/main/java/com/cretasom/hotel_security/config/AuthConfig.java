5d03142c0020d9ad6963b4443ce129cafddb3460
crestasom <41513543+shrest0000@users.noreply.github.com>
2024-08-02 10:22:48 +0545

    added oauth login via google

--- hotel-security/src/main/java/com/cretasom/hotel_security/config/AuthConfig.java
+++ hotel-security/src/main/java/com/cretasom/hotel_security/config/AuthConfig.java
@@ -9,16 +9,36 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class AuthConfig {
+	private static final String JWK_SET_URI = "https://your-auth-server/.well-known/jwks.json";
 
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		return http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(
-				requests -> requests.requestMatchers("/auth/token", "/auth/validate/**").permitAll()).build();
+		return http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(requests -> requests.requestMatchers("/auth/token", "/auth/validate/**",
+						"/auth/oauth2/authorize/github", "/oauth2/callback/github").permitAll())
+				.oauth2ResourceServer(
+						oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
+				.build();
+	}
+
+	@Bean
+	JwtAuthenticationConverter jwtAuthenticationConverter() {
+		JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+		// Customize the converter if needed
+		return converter;
+	}
+
+	@Bean
+	JwtDecoder jwtDecoder() {
+		return NimbusJwtDecoder.withJwkSetUri(JWK_SET_URI).build();
 	}
 
 	@SuppressWarnings("deprecation")
--- hotel-security/src/main/java/com/cretasom/hotel_security/controller/AuthController.java
+++ hotel-security/src/main/java/com/cretasom/hotel_security/controller/AuthController.java
@@ -1,6 +1,9 @@
 package com.cretasom.hotel_security.controller;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +16,17 @@ import com.cretasom.hotel_security.dto.AuthRequest;
 import com.cretasom.hotel_security.dto.AuthResp;
 import com.cretasom.hotel_security.service.AuthService;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 	@Autowired
 	private AuthService service;
+	@Value("${github.redirect.uri}")
+	private String REDIRECT_URI;
+	@Value("${github.client.id}")
+	private String CLIENT_ID;
 
 	@PostMapping("/token")
 	public ResponseEntity<AuthResp> getToken(@RequestBody AuthRequest authRequest) {
@@ -29,5 +38,13 @@ public class AuthController {
 	public ResponseEntity<Integer> validateToken(@PathVariable String token) {
 		int respCode = service.validateToken(token);
 		return ResponseEntity.status(200).body(respCode);
+	}
+
+	@GetMapping("/oauth2/authorize/github")
+	public void authorize(HttpServletResponse response) throws IOException {
+		String authorizationUrl = "https://github.com/login/oauth/authorize" + "?client_id=" + CLIENT_ID
+		// + "&redirect_uri=" + REDIRECT_URI
+				+ "&scope=read:user";
+		response.sendRedirect(authorizationUrl);
 	}
 }
